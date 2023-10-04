9776f8b9e8c1d65bcb17d7a07401c19b6f6bf4a9
Destii26 <zulfayanidesti@gmail.com>
2023-10-04 14:11:21 +0700

    frist commit

---
+++ login.php
@@ -0,0 +1,120 @@
+<?php
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Simpan logic autentikasi di sini
+    // Contoh sederhana:
+    if ($email == "zulfayanidesti@gmail.com" && $password == "1234567") {
+        // Autentikasi berhasil, arahkan pengguna ke halaman daftar produk
+        header("Location:listingProduk.php");
+        exit(); // Pastikan untuk keluar setelah pengalihan
+    } else {
+        // Autentikasi gagal, tampilkan pesan kesalahan atau lakukan tindakan lain
+        echo "Login failed. Please check your credentials.";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>form login php</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href=".//https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"/>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../html-dasar/plugins/fontawesome-free/css/all.min.css"/>
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="../html-dasar/plugins/icheck-bootstrap/icheck-bootstrap.min.css"/>
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../html-dasar/dist/css/adminlte.min.css"/>
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="index2.html" class="h1"><b>Gallery titi</b></a>
+        </div>
+        <div class="card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+
+            <?php
+            // Handle form submission
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+
+                // Perform necessary actions with the submitted data
+                // (e.g., validate, authenticate, etc.)
+            }
+            ?>
+
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="Email" name="email" required/>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Password" name="password" required/>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember" name="remember"/>
+                            <label for="remember"> Remember Me </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">
+                            Sign In
+                        </button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
+            <div class="social-auth-links text-center mt-2 mb-3">
+                <a href="#" class="btn btn-block btn-primary">
+                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                </a>
+                <a href="#" class="btn btn-block btn-danger">
+                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                </a>
+            </div>
+            <!-- /.social-auth-links -->
+
+            <p class="mb-1">
+                <a href="forgot-password.html">I forgot my password</a>
+            </p>
+            <p class="mb-0">
+                <a href="register.html" class="text-center">Register a new membership</a>
+            </p>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="../html-dasar/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../html-dasar/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../html-dasar/dist/js/adminlte.min.js"></script>
+</body>
+</html>
